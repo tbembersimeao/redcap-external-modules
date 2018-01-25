6798e62fcaca0330d2085fad3c04728131496ede
bascome <eva.bascompte.moragas@vanderbilt.edu>
2018-01-25 08:51:09 -0600

    deleteAllDAGRecords added value = $dagId

--- classes/AbstractExternalModule.php
+++ classes/AbstractExternalModule.php
@@ -795,11 +795,11 @@ class AbstractExternalModule
         $pid = db_escape(self::requireProjectId());
         $dagId = db_escape($dagId);
 
-        $records = $this->query("SELECT record FROM redcap_data where project_id = $pid and field_name = '__GROUPID__'");
+        $records = $this->query("SELECT record FROM redcap_data where project_id = $pid and field_name = '__GROUPID__' and value = $dagId");
         while ($row = db_fetch_assoc($records)){
             $this->query("DELETE FROM redcap_data where project_id = $pid and record = ".$row['record']);
         }
-        $this->query("DELETE FROM redcap_data where project_id = $pid and field_name = '__GROUPID__'");
+        $this->query("DELETE FROM redcap_data where project_id = $pid and field_name = '__GROUPID__' and value = $dagId");
     }
 
     public function deleteAllDAGUsers($dagId){
