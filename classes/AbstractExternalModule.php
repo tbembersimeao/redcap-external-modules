bc164e08755095a700c2990bacbd0c4c7900ca98
Mark McEver <mark@fastmail.net>
2018-05-22 09:48:37 -0500

    Fixed a missing parameter issue with the redcap_module_link_check_display() hook

--- classes/AbstractExternalModule.php
+++ classes/AbstractExternalModule.php
@@ -1070,7 +1070,7 @@ class AbstractExternalModule
 		ExternalModules::exitAfterHook();
 	}
 
-	public function redcap_module_link_check_display($link)
+	public function redcap_module_link_check_display($project_id, $link)
 	{
 		if (SUPER_USER) {
 			return $link;
